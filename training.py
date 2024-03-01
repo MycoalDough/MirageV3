c58ef12e23b976e15d74b4a936146202d670a5c9
Michael Do <112127345+MycoalDough@users.noreply.github.com>
2024-03-01 00:06:05 -0800

    Update training.py
    removal

--- training.py
+++ training.py
@@ -8,12 +8,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description='This is a demonstration program')
 
-# Here we add an argument to the parser, specifying the expected type, a help message, etc.
 parser.add_argument('-batch_size', type=str, required=True, help='Please provide a batch_size')
 
 args = parser.parse_args()
 
-# Now we can use the argument value in our program.
 print(f'batch size: {args.batch_size}')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -41,23 +39,18 @@ int_to_string = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [string_to_int[c] for c in s]
 decode = lambda l: ''.join([int_to_string[i] for i in l])
 
-# memory map for using small snippets of text from a single file of any size
 def get_random_chunk(split):
     filename = "train_split.txt" if split == 'train' else "val_split.txt"
     with open(filename, 'rb') as f:
         with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
-            # Determine the file size and a random position to start reading
             file_size = len(mm)
             start_pos = random.randint(0, (file_size) - block_size*batch_size)
 
-            # Seek to the random position and read the block of text
             mm.seek(start_pos)
             block = mm.read(block_size*batch_size-1)
 
-            # Decode the block to a string, ignoring any invalid byte sequences
             decoded_block = block.decode('utf-8', errors='ignore').replace('\r', '')
             
-            # Train and test splits
             data = torch.tensor(encode(decoded_block), dtype=torch.long)
             
     return data
@@ -98,24 +91,18 @@ class Head(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        # input of size (batch, time-step, channels)
-        # output of size (batch, time-step, head size)
+
         B,T,C = x.shape
-        k = self.key(x)   # (B,T,hs)
-        q = self.query(x) # (B,T,hs)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
-        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        k = self.key(x) 
+        q = self.query(x) 
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) 
+        wei = F.softmax(wei, dim=-1) 
         wei = self.dropout(wei)
-        # perform the weighted aggregation of the values
-        v = self.value(x) # (B,T,hs)
-        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        v = self.value(x) 
+        out = wei @ v 
         return out
 
-# [1, 0, 0]
-# [1, 0.6, 0]
-# [1, 0.6, 0.4]
 class MultiHeadAttention(nn.Module):
     """ multiple heads of self-attention in parallel """
 
@@ -126,7 +113,7 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        out = torch.cat([h(x) for h in self.heads], dim=-1) # (B, T, F) -> (B, T, [h1, h1, h1, h1, h2, h2, h2, h2, h3, h3, h3, h3])
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.dropout(self.proj(out))
         return out
     
@@ -150,7 +137,6 @@ class Block(nn.Module):
     """ Transformer block: communication followed by computation """
 
     def __init__(self, n_embd, n_head):
-        # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
@@ -171,7 +157,7 @@ class GPTLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
         
         
@@ -190,13 +176,12 @@ class GPTLanguageModel(nn.Module):
         B, T = index.shape
         
         
-        # idx and targets are both (B,T) tensor of integers
-        tok_emb = self.token_embedding_table(index) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
-        x = tok_emb + pos_emb # (B,T,C)
-        x = self.blocks(x) # (B,T,C)
-        x = self.ln_f(x) # (B,T,C)
-        logits = self.lm_head(x) # (B,T,vocab_size)
+        tok_emb = self.token_embedding_table(index)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+        x = tok_emb + pos_emb 
+        x = self.blocks(x)
+        x = self.ln_f(x) 
+        logits = self.lm_head(x) 
         
         if targets is None:
             loss = None
@@ -209,19 +194,14 @@ class GPTLanguageModel(nn.Module):
         return logits, loss
     
     def generate(self, index, max_new_tokens):
-        # index is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
 
             # get the predictions
             logits, loss = self.forward(index)
-            # focus only on the last time step
-            logits = logits[:, -1, :] # becomes (B, C)
-            # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1) # (B, C)
-            # sample from the distribution
-            index_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-            # append sampled index to the running sequence
-            index = torch.cat((index, index_next), dim=1) # (B, T+1)
+            logits = logits[:, -1, :] 
+            probs = F.softmax(logits, dim=-1) 
+            index_next = torch.multinomial(probs, num_samples=1) 
+            index = torch.cat((index, index_next), dim=1)
         return index
 
 model = GPTLanguageModel(vocab_size)
@@ -232,7 +212,6 @@ print('loaded successfully!')
 m = model.to(device)
 
 
-# create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for iter in range(max_iters):
@@ -241,7 +220,6 @@ for iter in range(max_iters):
         #losses = estimate_loss()
         #print(f"step: {iter}, train loss: {losses['train']:.3f}, val loss: {losses['val']:.3f}")
 
-    # sample a batch of data
     xb, yb = get_batch('train')
 
     # evaluate the loss
